80f98d943188c89cd5cd896e938c87706a85ce24
cxnaive <1012033254@qq.com>
2023-12-05 14:03:01 +0800

    add record & rules

--- bags/record.sh
+++ bags/record.sh
@@ -1,1 +1,1 @@
-ros2 bag record -o test_stable /cam_rgb/compressed /cam_thermal/compressed /ouster/imu /ouster/points /radar /radar/enhanced /tf
+ros2 bag record -o car_park_1 /cam_rgb/compressed /cam_thermal/compressed /ouster/imu /ouster/points /radar /radar/enhanced /tf
